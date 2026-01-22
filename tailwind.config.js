8fb552683fd49358a86b68f51da20b661f4dfa3e
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-22 21:44:07 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'SF Pro Display', 'Segoe UI', 'Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
